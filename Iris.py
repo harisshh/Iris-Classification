0e80469333f58d24f0aceef54f05b5bb39de8505
harisshh <66348607+harisshh@users.noreply.github.com>
2020-11-26 14:23:50 +0530

    Update Iris.py

--- Iris.py
+++ Iris.py
@@ -1,28 +1,28 @@
-//Importing the necessary packages
+# Importing the necessary packages
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 
-//Read the contents from the dataset
+# Read the contents from the dataset
 df = pd.read_csv('Iris.csv')
 df.describe()
 
-//Depict the same in graphs
+# Depict the same in graphs
 df['PetalWidthCm'].plot.hist()
 plt.show()
 
 sns.pairplot(df, hue='Species')
 
-//Separate the input and target in two variables
+# Separate the input and target in two variables
 all_inputs = df[['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm']].values
 all_classes = df['Species'].values
 
-//Separate testing and training data
+# Separate testing and training data
 (train_inputs, test_inputs, train_classes, test_classes) = train_test_split(all_inputs, all_classes, train_size=0.7, random_state=1)
 
-//Use Decision Tee Algorithm to classify the flowers
+# Use Decision Tee Algorithm to classify the flowers
 dtc = DecisionTreeClassifier()
 dtc.fit(train_inputs, train_classes)
 dtc.score(test_inputs, test_classes)
